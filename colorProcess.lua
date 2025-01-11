036be32bb8765b2d6ce1517adbb90c506dfb7464
zeykatecool <68609058+zeykatecool@users.noreply.github.com>
2025-01-11 21:49:12 +0300

    Update colorProcess.lua
    Random Color

--- colorProcess.lua
+++ colorProcess.lua
@@ -35,10 +35,21 @@ end
 
 _G.Color = {}
 
+local function randomHEXAColor()
+    local r,g,b = math.random(0,255),math.random(0,255),math.random(0,255)
+    return (r << 24) | (g << 16) | (b << 8) | 255
+end
+
 function Color.fromRGBA(R,G,B,A)
+    if type(R) ~= "number" or type(G) ~= "number" or type(B) ~= "number" or type(A) ~= "number" then
+        return randomHEXAColor()
+    end
     return (R << 24) | (G << 16) | (B << 8) | math.floor(A)
 end
 
 function Color.fromHEX(hex)
+    if type(hex) ~= "string" then
+        return hexA(randomHEXAColor())
+    end
     return hexA(hex)
 end
